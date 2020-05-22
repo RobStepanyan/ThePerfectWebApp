210d1c01c3442a7c9b0a33be6bc3bd58f657f3c0
RobStepanyan <robert1stepanyan@gmail.com>
2020-05-22 16:18:39 +0400

    Models: Repo model

--- Container/backend/main_app/models.py
+++ Container/backend/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+class Repo(models.Model):
+    full_name = models.CharField(max_length=150)
+    private = models.BooleanField()
+    owner = JSONField() # owner['login'] is a username of owner
+    html_url = models.URLField()
+    description = models.CharField(max_length=200, null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    last_updated = models.DateTimeField(auto_now=True)
