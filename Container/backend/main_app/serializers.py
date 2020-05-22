31d7365179cccf08c82e3617a9821fef974567f4
RobStepanyan <robert1stepanyan@gmail.com>
2020-05-22 16:18:31 +0400

    REST API: Init setup, retrieve, fetch_and_retrieve

---
+++ Container/backend/main_app/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Repo
+
+class RepoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Repo
+        fields = '__all__'
--- Container/backend/main_app/views.py
+++ Container/backend/main_app/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Repo
+from .serializers import RepoSerializer
+import requests, json
+from django.http import Http404
 
-# Create your views here.
-def home(request):
-    return render(request, 'main_app/home.html')
+class RepoViewSet(viewsets.ViewSet):
+    def fetch_and_retrieve(self, request, username):
+        req = requests.get(f'https://api.github.com/users/{username}/repos')
+        if 'message' in req.json() and req.json()['message'] == 'Not Found':
+            raise Http404() 
+        for repo in req.json():
+            Repo(
+                full_name=repo['full_name'],
+                private=repo['private'],
+                owner=repo['owner'],
+                html_url=repo['html_url'],
+                description=repo['description'],
+                created_at=repo['created_at'],
+                updated_at=repo['updated_at'],
+            ).save()
+        __class__.retrieve(request, request, username)
+
+    def retrieve(self, request, username):
+        queryset = Repo.objects.filter(owner__login__iexact = username) or None
+        if queryset:
+            serializer = RepoSerializer(queryset, many=True)
+            return Response(serializer.data)
+        else:
+            __class__.fetch_and_retrieve(self, request, username)
